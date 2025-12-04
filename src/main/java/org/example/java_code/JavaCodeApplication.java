542dc2578be9688e19f514ec4c17efd3b5483832
zhilong.zhang <zhilong.zhang@shopee.com>
2025-12-04 15:44:33 +0800

    update

--- src/main/java/org/example/java_code/JavaCodeApplication.java
+++ src/main/java/org/example/java_code/JavaCodeApplication.java
@@ -2,8 +2,10 @@ package org.example.java_code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavaCodeApplication {
 
 	public static void main(String[] args) {
